e56c39577e97c36e1bf4d70518fe531fa32cdced
Võ Thị Kiều Oanh <21dh111358@st.huflit.edu.vn>
2024-03-17 20:45:06 +0700

    message

---
+++ BeautyStore/Strategy/LayoutHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautyStore.Strategy
+{
+    public static class LayoutHelper
+    {
+        public static string GetThemeStyleSheet()
+        {
+            var theme = "light"; // Chọn chế độ mặc định là sáng
+            if (HttpContext.Current.Request.Cookies["theme"] != null)
+            {
+                theme = HttpContext.Current.Request.Cookies["theme"].Value;
+            }
+            return $"/Content/css/{theme}-theme.css";
+        }
+    }
+
+}
